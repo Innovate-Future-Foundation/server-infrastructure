8dafc24af21283d80157b7691ea8c79d7423fc41
Yc Wang <swyc168@gmail.com>
2025-02-08 23:56:10 +1100

    feat: add route table for private subnet

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
   tags = {
-    Name = "public-rt"
+    Name = "${var.vpc_name}-public-rt"
   }
 }
 
@@ -49,4 +49,19 @@ resource "aws_route_table_association" "public" {
   for_each       = aws_subnet.public
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
+}
+
+# For private subnets (if using NAT Gateway later)
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each       = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
 }
