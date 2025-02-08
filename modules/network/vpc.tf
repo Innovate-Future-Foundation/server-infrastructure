1f76d4083e650fe4440e4e465a0f850a5d5ae90c
Yc Wang <swyc168@gmail.com>
2025-02-08 15:22:55 +1100

    fix: add missing route table association

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -44,3 +44,9 @@ resource "aws_route_table" "public" {
     Name = "public-rt"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
