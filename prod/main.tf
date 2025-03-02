1acba34ba683d77060e9b2e44c5e3337cdea3595
YC Wang <swyc168@gmail.com>
2025-03-03 00:41:21 +1100

    feat: use new ecs modules and provision ecr repo policy for ecs role (#16)
    * feat: use new ecs modules and provision ecr repo policy for ecs role
    
    * chore: rename prod tf plan workflow
    
    * fix: conduct repo uri instead
    
    * fix: typo

--- prod/main.tf
+++ prod/main.tf
@@ -1,42 +1,97 @@
 locals {
+  # Central ECR URI
+  base_repo    = "inff/backend-publish"
+  base_uri     = "${var.prod_account_id}.dkr.ecr.${var.ecr_region}.amazonaws.com/${local.base_repo}"
+  publish_repo = "inff/backend-base"
+  publish_uri  = "${var.prod_account_id}.dkr.ecr.${var.ecr_region}.amazonaws.com/${local.publish_repo}"
+
   # Network Settings
   vpc = {
-    main = {
-      vpc_name = "inff-prod-main"
-      cidr     = "10.1.0.0/16"
-      public_subnets = {
-        api-a-subnet = {
-          cidr = "10.1.0.0/20"
-          az   = "us-west-2a"
-        }
-        api-b-subnet = {
-          cidr = "10.1.16.0/20"
-          az   = "us-west-2b"
-        },
-        tool-subnet = {
-          cidr = "10.1.32.0/20"
-          az   = "us-west-2a"
-        },
+    vpc_name = "inff-prod-main"
+    cidr     = "10.1.0.0/16"
+    public_subnets = {
+      api-a-subnet = {
+        cidr = "10.1.0.0/20"
+        az   = "us-west-2a"
       }
-      private_subnet = {
-        preserved-subnet = {
-          cidr = "10.1.48.0/20"
-          az   = "us-west-2a"
-        },
+      api-b-subnet = {
+        cidr = "10.1.16.0/20"
+        az   = "us-west-2b"
+      },
+      tool-subnet = {
+        cidr = "10.1.32.0/20"
+        az   = "us-west-2a"
+      },
+    }
+    private_subnet = {
+      preserved-subnet = {
+        cidr = "10.1.48.0/20"
+        az   = "us-west-2a"
+      },
+    }
+  }
+
+  # ECS Cluster and Task Definition Settings
+  main_ecs_def = {
+    cluster_name = "inff-prod-backend"
+    families = {
+      backend = {
+        role = "inff-backend-ecs-role"
+        name = "inff-prod-api"
+        cpu  = 1024
+        mem  = 2048
+        # Containers Definition
+        containers = templatefile("backend-task-def-template.json", {
+          backend_base_repo    = local.base_uri
+          backend_publish_repo = local.publish_uri
+          # Container Envs
+          db_user    = "db_admin"
+          db_pass    = "123321aab@"
+          db_name    = "InnovateFuture"
+          jwt_secret = "5-3218)7v*qX3CN2"
+          dep_env    = "Development"
+          # Logging Settings
+          logs_region = var.region
+          logs_group  = module.cloudwatch.log_group_names["ecs_default"]
+        })
       }
     }
   }
-  # ECS Settings
+
+  # ECS Service
+  main_ecs_srv = {
+    name    = "api-srv"
+    cluster = module.main_ecs_def.cluster_id
+    family  = module.main_ecs_def.task_family_arns["backend"]
+    network = {
+      subnets = [
+        module.network.public_subnet_ids["api-a-subnet"],
+        module.network.public_subnet_ids["api-b-subnet"]
+      ]
+      security_groups = [
+        module.network.security_group_ids["backend"]
+      ]
+      assign_pub_ip = true
+    }
+
+    srv_registry = {
+      registry_arn   = module.cloud_map.service_arns["backend"]
+      container_name = "api"
+      container_port = 5091
+    }
+  }
+
+  ecs_logs_group          = "inff/ecs/default_log_group"
   task_execution_role     = "inff-backend-ecs-role"
   central_ecr_repo_policy = data.aws_iam_policy_document.central_ecr_repo_policy.json
 }
 
 module "network" {
   source          = "../modules/network"
-  vpc_name        = "${local.vpc.main.vpc_name}-vpc"
-  vpc_cidr        = local.vpc.main.cidr
-  public_subnets  = local.vpc.main.public_subnets
-  private_subnets = local.vpc.main.private_subnet
+  vpc_name        = "${local.vpc.vpc_name}-vpc"
+  vpc_cidr        = local.vpc.cidr
+  public_subnets  = local.vpc.public_subnets
+  private_subnets = local.vpc.private_subnet
 
   security_groups = {
     backend = {
@@ -47,18 +102,18 @@ module "network" {
         to_port   = 5091
         protocol  = "tcp"
         cidr_blocks = [
-          local.vpc.main.public_subnets.api-a-subnet.cidr,
-          local.vpc.main.public_subnets.api-b-subnet.cidr,
+          local.vpc.public_subnets.api-a-subnet.cidr,
+          local.vpc.public_subnets.api-b-subnet.cidr,
         ] }, {
         from_port = 5432
         to_port   = 5432
         protocol  = "tcp"
         cidr_blocks = [
-          local.vpc.main.public_subnets.api-a-subnet.cidr,
-          local.vpc.main.public_subnets.api-b-subnet.cidr,
-          local.vpc.main.public_subnets.tool-subnet.cidr
-        ] }
-      ]
+          local.vpc.public_subnets.api-a-subnet.cidr,
+          local.vpc.public_subnets.api-b-subnet.cidr,
+          local.vpc.public_subnets.tool-subnet.cidr
+        ]
+      }]
     },
   }
 }
@@ -83,6 +138,31 @@ module "cloud_map" {
   }
 }
 
+module "cloudwatch" {
+  source = "../modules/cloudwatch_logs"
+  log_groups = {
+    ecs_default = {
+      name      = local.ecs_logs_group
+      retention = 14
+    }
+  }
+}
+
+module "main_ecs_def" {
+  source       = "../modules/ecs-def"
+  cluster_name = local.main_ecs_def.cluster_name
+  families     = local.main_ecs_def.families
+}
+
+module "main_ecs_srv" {
+  source       = "../modules/ecs-srv"
+  name         = local.main_ecs_srv.name
+  cluster      = module.main_ecs_def.cluster_id
+  family       = local.main_ecs_srv.family
+  network      = local.main_ecs_srv.network
+  srv_registry = local.main_ecs_srv.srv_registry
+}
+
 module "ecr" {
   providers = { aws = aws.central_ecr }
   source    = "../modules/ecr"
@@ -99,4 +179,7 @@ module "ecr" {
       policy      = local.central_ecr_repo_policy
     }
   }
+
+  # Must provision after task role (including that of other envs)
+  depends_on = [module.main_ecs_def]
 }
